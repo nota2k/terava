8d7116837bf39826dad6cc03eebf0eb04c3ad1ae
Nelly <nelly.babillon@gmail.com>
2025-04-17 17:00:02 +0200

    feat: Ajout du modèle Trip, des migrations, des factories et des requêtes pour la gestion des voyages

---
+++ database/migrations/2025_04_17_144106_create_trips_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::disableForeignKeyConstraints();
+
+        Schema::create('trips', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->longText('description');
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->string('location');
+            $table->foreignId('user_id')->constrained();
+            $table->timestamps();
+        });
+
+        Schema::enableForeignKeyConstraints();
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('trips');
+    }
+};
